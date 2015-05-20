1ce1225f8feed29af4f4f2c804d74b007b1421e1
Dan Comand <dcomand@gmail.com>
2015-05-20 00:10:13 -0400

    Creating sessions models and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
 
 
+
  root 'bookmarks#index'
 
  resources :bookmarks
  resources :users, only: [:new, :create]
+ resources :sessions, only: [:new, :create, :destroy]
 
 end
