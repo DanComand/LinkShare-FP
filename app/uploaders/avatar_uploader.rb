d21c320b7bb5367649d7fda9a3c69e2bceeb478d
Dan Comand <dcomand@gmail.com>
2015-05-22 21:44:36 -0400

    Made some navigation and UX changes.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -37,7 +37,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [70, 70]
+    process :resize_to_fit => [32, 32]
+  end
+  version :fifty do  
+    process :resize_to_fit => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
